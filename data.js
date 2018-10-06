08386dc4ed967a21100c30abf50e3b619d7a6726
Brian <brian.young@csedge.org>
2018-10-06 15:45:38 -0400

    Completed Challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,31 @@
-// Example
-let message = "Hello World!";
-console.log(message);
+//numbers
+let HP 
+HP = 512;
+let ATK
+ATK = 242;
+let SPD
+SPD = 67;
 
-// Type your code below!
+//strings
+let GOOFY = ("best girl");
+let MICKEY = ("lord evil mouse");
+let DONALD = ("some duck");
+
+//booleans
+let GoofyIsLove = true;
+let MickeyMouseIsBad = true;
+let DonaldDuckIsOverrated = false;
+
+//undefined
+console.log(HEALTH = HP);
+console.log(ATTACK = ATK);
+console.log(SPEED = SPD);
+
+const READLINE = require("readline-sync");
+
+let userInput = READLINE.question("Type anything and I'll repeat it back!");
+
+console.log(userInput);
+
+
+
