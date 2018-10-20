4c6164a04520e3e3a4c1b1d7de64b3c9dd3a82f1
Brian <brian.young@csedge.org>
2018-10-20 12:17:09 -0400

    Completed objects.js

---
+++ objects.js
@@ -0,0 +1,22 @@
+let villainGroup = {
+	groupName: "Paul Brothers",
+	villainCatchPhrase: "It's everyday bro",
+	worstCrime: "Suicide forest"
+
+
+};
+
+
+let hacker = {
+	name: "XxUnknown",
+	hackerCatchPhrase: "SCREECH",
+	archEnemy: "Omar"
+};
+
+console.log(`
+	THE HACKER ${hacker.name}'s catchphrase is ${hacker.hackerCatchPhrase}. 
+	Their archenemy is ${hacker.archEnemy}. 
+	They fight against the evil group, the ${villainGroup.groupName}, 
+	whose catchphrase is ${villainGroup.villainCatchPhrase}, 
+	and their worst atrocity is the filming of ${villainGroup.worstCrime} 
+	`);
