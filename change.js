064609fd4e298ff861899b4979de49f8c681eed2
Brian <brian.young@csedge.org>
2018-10-20 15:13:15 -0400

    Completed change.js

--- change.js
+++ change.js
@@ -2,6 +2,41 @@
 const READLINE = require("readline-sync");
 
 // fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+let changeMachine = {
+	inputCents: undefined,
+	quarters: undefined,
+	dimes: undefined,
+	nickels: undefined,
+	pennies: undefined,
+	greeting: "Moshi moshi, ATM desu!",
+	farewell: "Arigato gozaimashita!"
+};
 
 // write code below that simulates the change machine in action!
+console.log(`
+***************************************************
+${changeMachine.greeting}`);
+
+changeMachine.inputCents = READLINE.question(
+'Enter the amount of cents: ');
+
+changeMachine.quarters = Math.floor(changeMachine.inputCents / 25);
+let y = changeMachine.inputCents % 25;
+
+changeMachine.dimes = Math.floor(y / 10);
+let y1 = y % 10;
+
+changeMachine.nickels = Math.floor(y1 / 5);
+
+changeMachine.pennies = y1 % 5;
+
+console.log(`
+quarters = ${changeMachine.quarters}
+dimes = ${changeMachine.dimes}
+nickels = ${changeMachine.nickels}
+pennies = ${changeMachine.pennies}
+
+${changeMachine.farewell}
+***************************************************
+`);
+
